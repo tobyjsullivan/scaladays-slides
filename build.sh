c7a357fca7515d6f0b8e57b2d23b89c74b70deed
Toby Sullivan <toby.sullivan@hootsuite.com>
2015-05-28 17:04:26 -0700

    Fix build.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo "Installing bower components..."
 bower install
 
 echo "Building css..."
+sass css/theme/source/hootsuite.scss css/theme/hootsuite.css
 sass --update css/theme/source/:css/theme/
 
 echo "Done!"
